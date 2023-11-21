37b6426d2ad5a9ede41295c91d5b7edcc0ab09e9
Patrick <patrick.attimont@grenoble-inp.org>
2023-11-21 23:33:22 +0100

    Add diferent types of events

---
+++ nexus/src/nexus/Events/Event.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+#include <functional>
+
+namespace Nexus {
+
+    enum class EventType {
+        None = 0,
+        WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+        AppTick, AppUpdate, AppRender,
+        KeyPressed, KeyReleased,
+        MouseButtonPressed, MousButtonReleased, MouseMoved, MouseScrolled
+    };
+
+    enum EventCategory {
+        None = 0,
+        EventCategoryApplication    = BIT(0),
+        EventCategoryInput          = BIT(1),
+        EventCategoryKeyboard       = BIT(2),
+        EventCategoryMouse          = BIT(3),
+        EventCategoryMouseButton    = BIT(4)
+    };
+
+    #define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+                                    virtual EventType GetEventType() const override { return GetStaticType(); }\
+                                    virtual const char *GetName() const override { return #type; }
+
+    #define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+    class __attribute__((visibility("default"))) Event {
+        friend class EventDispatcher;
+
+    public:
+        virtual EventType GetEventType() const = 0;
+        virtual const char *GetName() const = 0;
+        virtual int GetCategoryFlags() const = 0;
+        virtual std::string ToString() const { return GetName(); }
+
+        inline bool IsInCategory(EventCategory category) {
+            return GetCategoryFlags() & category;
+        }
+        
+    protected:
+        bool m_Handled = false;
+    };
+}
