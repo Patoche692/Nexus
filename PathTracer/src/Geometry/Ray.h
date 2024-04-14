8a59727f5961908569fd1e3e6870c57f1f91bf22
Patrick Attimont <patrickattimont@gmail.com>
2024-04-14 14:30:26 +0200

    Added members to Ray class for ray traversal and intersection

--- PathTracer/src/Geometry/Ray.h
+++ PathTracer/src/Geometry/Ray.h
@@ -2,19 +2,26 @@
 #include "Utils/cuda_math.h"
 #include "Medium.h"
 
-// Make it a struct so that its properties are publicly available to CUDA without setters / getters
-
 struct Ray
 {
 	__host__ __device__ Ray() = default;
 	__host__ __device__ Ray(float3 o, float3 d)
 		:origin(o), direction(d) {};
 
+	// Ray origin
+	float3 origin = make_float3(0.0f);
+
+	// Ray direction
+	float3 direction = make_float3(0.0f, 0.0f, 1.0f);
+
+	// Ray inverse direction (reduce divisions for optimization)
+	float3 invDirection = make_float3(0.0f, 0.0f, 1.0f);
+
+	// Ray hit distance
+	float t = 0.0f;
+
+	// Medium
+	Medium medium = { 1.0f };
+
 	inline __host__ __device__ float3 PointAtParameter(float t) const { return origin + direction * t; };
-
-	float3 origin;
-	float3 direction;
-
-	Medium medium;
-	bool transmitted;
 };
