97dd7ae6fec1ac667847d981345231299cfb8905
Patrick Attimont <patrickattimont@gmail.com>
2024-04-14 19:29:13 +0200

    AABB implementation

--- PathTracer/src/Geometry/AABB.h
+++ PathTracer/src/Geometry/AABB.h
@@ -28,4 +28,16 @@ struct AABB
 		float3 diff = bMax - bMin;
 		return diff.x * diff.y + diff.y * diff.z + diff.x * diff.z;
 	}
+
+	__host__ __device__ static inline float intersectionAABB(const Ray& ray, const float3 bMin, const float3 bMax)
+	{
+		float tx1 = (bMin.x - ray.origin.x) * ray.invDirection.x, tx2 = (bMax.x - ray.origin.x) * ray.invDirection.x;
+		float tmin = min(tx1, tx2), tmax = max(tx1, tx2);
+		float ty1 = (bMin.y - ray.origin.y) * ray.invDirection.y, ty2 = (bMax.y - ray.origin.y) * ray.invDirection.y;
+		tmin = max(tmin, min(ty1, ty2)), tmax = min(tmax, max(ty1, ty2));
+		float tz1 = (bMin.z - ray.origin.z) * ray.invDirection.z, tz2 = (bMax.z - ray.origin.z) * ray.invDirection.z;
+		tmin = max(tmin, min(tz1, tz2)), tmax = min(tmax, max(tz1, tz2));
+		if (tmax >= tmin && tmin < ray.t && tmax > 0) return tmin; else return 1e30f;
+	}
 };
+
