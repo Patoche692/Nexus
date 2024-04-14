f5bdccee7d5c895195cbd30a5648c9a781a4a443
Patrick Attimont <patrickattimont@gmail.com>
2024-04-14 14:29:47 +0200

    Changed Triangle class to include centroids

--- PathTracer/src/Geometry/Triangle.h
+++ PathTracer/src/Geometry/Triangle.h
@@ -4,31 +4,31 @@
 
 struct Triangle
 {
+	// Positions
 	float3 pos0;
 	float3 pos1;
 	float3 pos2;
+	float3 centroid;
 
+	// Normals
 	float3 normal0;
 	float3 normal1;
 	float3 normal2;
 
+	// Texture coordinates
 	float3 texCoord0;
 	float3 texCoord1;
 	float3 texCoord2;
 
 	__host__ __device__ Triangle() = default;
 
-	__host__ __device__ Triangle(float3 p0, float3 p1, float3 p2)
-		:pos0(p0), pos1(p1), pos2(p2), normal0(make_float3(0.0f)), normal1(make_float3(0.0f)),
-		normal2(make_float3(0.0f)), texCoord0(make_float3(0.0f)), texCoord1(make_float3(0.0f)), texCoord2(make_float3(0.0f)) { }
-
-	__host__ __device__ Triangle(float3 p0, float3 p1, float3 p2, float3 n0, float3 n1, float3 n2)
-		:pos0(p0), pos1(p1), pos2(p2), normal0(n0), normal1(n1),
-		normal2(n2), texCoord0(make_float3(0.0f)), texCoord1(make_float3(0.0f)), texCoord2(make_float3(0.0f)) { }
-
-	__host__ __device__ Triangle(float3 p0, float3 p1, float3 p2, float3 n0, float3 n1, float3 n2, float3 t0, float3 t1, float3 t2)
-		:pos0(p0), pos1(p1), pos2(p2), normal0(n0), normal1(n1),
-		normal2(n2), texCoord0(t0), texCoord1(t1), texCoord2(t2) { }
+	__host__ __device__ Triangle(
+		float3 p0, float3 p1, float3 p2,
+		float3 n0 = make_float3(0.0f), float3 n1 = make_float3(0.0f), float3 n2 = make_float3(0.0f),
+		float3 t0 = make_float3(0.0f), float3 t1 = make_float3(0.0f), float3 t2 = make_float3(0.0f)
+	): pos0(p0), pos1(p1), pos2(p2), normal0(n0), normal1(n1),
+		normal2(n2), texCoord0(t0), texCoord1(t1), texCoord2(t2),
+		centroid((pos0 + pos1 + pos2) * 0.33333333f) { }
 
 
 	// Möller-Trumbore intersection algorithm. See https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
