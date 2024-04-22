2265f01c8c0f964f2166148a62b2e115284d21e4
Patrick Attimont <patrickattimont@gmail.com>
2024-04-22 21:56:49 +0200

    Removed transform from BVHInstance

--- PathTracer/src/Geometry/BVH/BVHInstance.h
+++ PathTracer/src/Geometry/BVH/BVHInstance.h
@@ -17,26 +17,12 @@ public:
 	void SetTransform(Mat4& t);
 	void SetTransform(float3 pos, float3 r, float3 s);
 
-	void SetPosition(float3 pos);
-
-	// Rotation is in degrees
-	void SetRotation(float3 axis, float angle);
-	void SetRotationX(float angle);
-	void SetRotationY(float angle);
-	void SetRotationZ(float angle);
-
-	void SetScale(float s);
-	void SetScale(float3 s);
-
 	void AssignMaterial(int mIdx);
 
 public:
 	BVH* bvh = nullptr;
 	Mat4 invTransform;
 	Mat4 transform;
-	float3 position = make_float3(0.0f);
-	float3 rotation = make_float3(0.0f);
-	float3 scale = make_float3(1.0f);
 	AABB bounds;
 	int materialId;
 
