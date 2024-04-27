0e89bbbfceba97d459cecc9bd9c49d93e85d31e5
NiklasHolmes <67655592+NiklasHolmes@users.noreply.github.com>
2024-04-27 10:37:10 +0200

    bsdf lambertian start

--- PathTracer/src/Cuda/BSDF.cuh
+++ PathTracer/src/Cuda/BSDF.cuh
@@ -1,6 +1,35 @@
 #pragma once
 #include <cuda_runtime_api.h>
 
+#define ONE_DIV_PI (0.31830988618)
+
 struct BSDF {
 
+	// for 
+	// lambertianeval => for diffuesd FUNCTION
+	// lambertianPDF FUNCTION
+
+	// material properties: 
+	float3 diffuseReflectance;
+	float3 specular;
+
+	float NdotL;
+
+
+	inline __device__ float lambertian(const BSDF data)
+	{
+		return 1.0f;
+	}
+
+	inline __device__ float3 lambertianEval() {
+		return ONE_DIV_PI * NdotL * diffuseReflectance;
+	}
+
+	inline __device__ float3 lambertianPDF(const BSDF data) {
+		
+	}
+
+	inline __device__ float diffusePdf(const BSDF data) {
+		return data.NdotL * ONE_DIV_PI;
+	}
 };
