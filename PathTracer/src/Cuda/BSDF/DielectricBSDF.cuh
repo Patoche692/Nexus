85d156eec8e5ceff373dfd7d2f3b857b66922c74
Patrick Attimont <patrickattimont@gmail.com>
2024-05-04 20:31:59 +0200

    Dieletric bsdf finished

--- PathTracer/src/Cuda/BSDF/DielectricBSDF.cuh
+++ PathTracer/src/Cuda/BSDF/DielectricBSDF.cuh
@@ -6,6 +6,10 @@
 #include "Microfacet.cuh"
 #include "Fresnel.cuh"
 
+/* 
+	Rough dielectric BSDF based on the paper "Microfacet Models for Refraction through Rough Surfaces"
+	See https://www.graphics.cornell.edu/~bjw/microfacetbsdf.pdf
+*/
 struct DielectricBSDF 
 {
 	float F0;
@@ -14,20 +18,21 @@ struct DielectricBSDF
 	float alpha;
 	float reflectance;
 
-	inline __device__ void PrepareBSDFData(float3& Vlocal,  Material& material)
+	inline __device__ void PrepareBSDFData(float3& wi,  Material& material)
 	{
-		alpha = material.roughness * material.roughness;
+		//alpha = max(material.roughness * material.roughness, 1.0e-3);
+		alpha = max(1.0e-3, (1.2f - 0.2f * sqrt(abs(wi.z))) * material.roughness);
 		reflectance = material.iorLevel;
 
 		// TODO: include etaI in ray structure
 		float etaI = 1.0f;
-		eta = Vlocal.z < 0 ? material.ior / etaI : etaI / material.ior;
+		eta = wi.z < 0.0f ? material.ior / etaI : etaI / material.ior;
 		F0 = (material.ior - etaI) / (etaI + material.ior);
 		F0 *= F0;
-		F0 *= reflectance * 2;
+		//F0 *= reflectance * 2;
 	}
 
-	inline __device__ bool Eval(HitResult& hitResult, float3& wi, float3& wo, float3& throughput, unsigned int& rngState)
+	inline __device__ bool Eval(HitResult& hitResult, float3& wi, float3& wo, float3& throughput, unsigned int& rngState, float3 gnormal)
 	{
 		float3 m;
 
@@ -39,9 +44,12 @@ struct DielectricBSDF
 		else
 			m = Microfacet::SampleSpecularHalfBeckWalt(alpha, rngState);
 
-		float wiDotM = dot(wi, m);
+		const float wiDotM = dot(wi, m);
 
-		float fr = Fresnel::SchlickDielectricReflectance(F0, Fresnel::ShadowedF90(make_float3(F0)), wiDotM);
+		float cosThetaT;
+		const float fr = Fresnel::DieletricReflectance(1 / hitResult.material.ior, wiDotM, cosThetaT);
+		bool sampleT = hitResult.material.transmittance > 0.0f;
+
 		float3 F = make_float3(fr);
 
 		// Randomly select a reflected or diffuse ray based on Fresnel reflectance
@@ -51,14 +59,14 @@ struct DielectricBSDF
 			wo = reflect(-wi, m);
 
 			// If the new ray is under the hemisphere, return
-			if (wo.z <= 0.0f)
+			if (wo.z * wi.z < 0.0f)
 				return false;
 
-			float wiDotM = dot(wi, m);
-			float weight = Microfacet::WeightBeckmannWalter(alpha, wiDotM, wo.z, wi.z, m.z);
+			const float wiDotM = dot(wi, m);
+			const float weight = Microfacet::WeightBeckmannWalter(alpha, abs(wiDotM), abs(wo.z), abs(wi.z), m.z);
 
 			// Handle divisions by zero
-			if (weight > 1e10)
+			if (weight > 1.0e10)
 				return false;
 
 			throughput = F * weight / fr;
@@ -69,19 +77,17 @@ struct DielectricBSDF
 			if (Random::Rand(rngState) < hitResult.material.transmittance)
 			{
 				// Transmission
-				wo = reflect(wi, m);
-				wo.z *= -1;
-				float woDotM = abs(dot(wo, m));
-				float weight = Microfacet::WeightBeckmannWalter(alpha, woDotM, wo.z, wi.z, m.z);
+				wo = (eta * wiDotM - Utils::SgnE(wiDotM) * cosThetaT) * m - eta * wi;
 
-				if (weight > 1e10)
+				if (wo.z * wi.z > 0.0f)
+					return false;
+
+				const float weight = Microfacet::WeightBeckmannWalter(alpha, abs(wiDotM), abs(wo.z), abs(wi.z), m.z);
+
+				if (weight > 1.0e10)
 					return false;
 
 				throughput = hitResult.material.diffuse * weight;
-
-				// If the new ray is in the upper hemisphere, return
-				if (wo.z >= 0.0f)
-					return false;
 			}
 			else
 			{
